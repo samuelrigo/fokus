c4a89823c2624ad92ee308ce782a1b2df9a6053f
samel <samuelrigo@gmail.com>
2023-09-05 09:38:56 -0300

    Made a const named title that selects the title on my html, and made a switch in my changecontext function that changes my title accordingly to the context

--- script.js
+++ script.js
@@ -3,10 +3,32 @@ const focoButton = document.querySelector('.app__card-button--foco')
 const curtoButton = document.querySelector('.app__card-button--curto')
 const longoButton = document.querySelector('.app__card-button--longo')
 const banner = document.querySelector('.app__image')
+const title = document.querySelector('.app__title')
 
 function changeContext(contexto){
     html.setAttribute('data-contexto', contexto)
     banner.setAttribute('src', `./imagens/${contexto}.png`)
+    switch (contexto) {
+        case "foco":
+            title.innerHTML = `
+            Otimize sua produtividade,<br>
+            <strong class="app__title-strong">mergulhe no que importa.</strong>
+            `
+        break;
+
+        case "descanso-curto": title.innerHTML = `
+            Que tal dar uma respirada? <strong class="app__title-strong">Faça uma pausa curta!</strong>
+            `
+        break;
+        
+        case "descanso-longo": title.innerHTML = `
+            Hora de voltar à superfície.<strong class="app__title-strong"> Faça uma pausa longa.</strong>
+            `
+        break;
+
+        default:
+            break;
+    }
 }
 
 focoButton.addEventListener('click', () => {
