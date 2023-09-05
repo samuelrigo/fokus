47944f142377c97b93fe3893e9bb18a22a13e3f8
samel <samuelrigo@gmail.com>
2023-09-05 17:12:51 -0300

    Fixed the highlight when you click on any context

--- script.js
+++ script.js
@@ -4,8 +4,25 @@ const curtoButton = document.querySelector('.app__card-button--curto')
 const longoButton = document.querySelector('.app__card-button--longo')
 const banner = document.querySelector('.app__image')
 const title = document.querySelector('.app__title')
+const botoes = document.querySelectorAll('.app__card-button')
+
+focoButton.addEventListener('click', () => {
+    changeContext('foco')
+    focoButton.classList.add('active')
+})
+curtoButton.addEventListener('click', () => {
+    changeContext('descanso-curto')
+    curtoButton.classList.add('active')
+})
+longoButton.addEventListener('click', () => {
+    changeContext('descanso-longo')
+    longoButton.classList.add('active')
+})
 
 function changeContext(contexto){
+    botoes.forEach(function(contexto){
+        contexto.classList.remove('active')
+    })
     html.setAttribute('data-contexto', contexto)
     banner.setAttribute('src', `./imagens/${contexto}.png`)
     switch (contexto) {
@@ -31,14 +48,5 @@ function changeContext(contexto){
     }
 }
 
-focoButton.addEventListener('click', () => {
-    changeContext('foco')
-})
-curtoButton.addEventListener('click', () => {
-    changeContext('descanso-curto')
-})
-longoButton.addEventListener('click', () => {
-    changeContext('descanso-longo')
-})
 
 
